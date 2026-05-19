22c152b0fd67e14d65110d4d5e59f39f8e706863
belcadilina-lab <belcadi.lina@etu.uae.ac.ma>
2026-05-19 09:41:24 +0100

    Update api.constants.ts

--- frontend/src/app/core/constants/api.constants.ts
+++ frontend/src/app/core/constants/api.constants.ts
@@ -1,1 +1,7 @@
-export const API_URL = 'http://127.0.0.1:8000/api';
+const apiHost =
+  typeof window !== 'undefined' && window.location.hostname
+    ? window.location.hostname
+    : '127.0.0.1';
+
+export const API_BASE_URL = `http://${apiHost}:8000`;
+export const API_URL = `${API_BASE_URL}/api`;
